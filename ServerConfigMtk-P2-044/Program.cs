54dfa0cc60e4e6245e8248c9720637fd15450623
Fakhrizar Rifqi Fuad <fakhrizar.rifqi.ft18@mail.umy.ac.id>
2020-10-17 14:21:58 +0700

    Add project files.

---
+++ ServerConfigMtk-P2-044/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigMtk_P2_044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
